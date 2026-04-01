09f09436aa11aec221db1bc908944857f6516534
mario-lsb <pathakaditi96@gmail.com>
2026-04-01 14:45:09 +0530

    Added UC10 Normalized Palindrome Checker

---
+++ UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "A man a plan a canal Panama";
+
+        // Normalize string: remove spaces & symbols, convert to lowercase
+        String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
